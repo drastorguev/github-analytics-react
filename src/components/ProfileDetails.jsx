d8482c7eed8f1c6e27f3b754fa4f631db76f6ff1
Dmitry Rastorguev <dmitry@rastorguev.co.uk>
2018-02-12 15:32:29 +0000

    final publication code

--- src/components/ProfileDetails.jsx
+++ src/components/ProfileDetails.jsx
@@ -51,8 +51,8 @@ const ProfileDetails = (props) => {
           {props.infoclean.html_url ? <div><p><a href={props.infoclean.html_url} target="_blank">View on GitHub</a></p></div> : null }
         </div>
         <div>
-          {/* {props.infoclean.login ? <div>{ <img src={"http://ghchart.rshah.org/"+props.infoclean.login} alt="Github chart" />
-        }<br/><a href="https://ghchart.rshah.org/" target="_blank">Source for GitHub Chart API</a></div> : null } */}
+          {props.infoclean.login ? <div>{ <img src={"http://ghchart.rshah.org/"+props.infoclean.login} alt="Github chart" />
+        }<br/><a href="https://ghchart.rshah.org/" target="_blank">Source for GitHub Chart API</a></div> : null }
         </div>
       </div>
     )
